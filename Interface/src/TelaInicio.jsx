b54cefe43c69ca4aff7c775df6aa67101511a63e
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-11-14 11:56:54 -0300

    finalização header

--- Interface/src/TelaInicio.jsx
+++ Interface/src/TelaInicio.jsx
@@ -1,19 +1,20 @@
-import"./TelaInicio.scss";
+import "./TelaInicio.scss";
 import { Link } from 'react-router-dom';
 
 const TelaInicio = () => {
   return (
-    <header className="header">
-        <div>
+    <header >
+      <div className="pginicio">
 
-        </div >
-        <div>
+      </div >
+      <div className="bt">
         <h1>VAMOS COMEÇAR! POR FAVOR ACESSE:</h1>
-      
-        <button > 
-            <Link to={`Organizador`} > ORGANIZADOR DE TAREFAS </Link> 
+
+        <button >
+          <Link id="lk" to={`Organizador`} > ORGANIZADOR DE TAREFAS </Link>
         </button></div>
-        
+        <div id="fim"></div>
+
     </header>
   )
 }
--- Interface/src/views/Organizador.jsx
+++ Interface/src/views/Organizador.jsx
@@ -5,146 +5,151 @@ import ConfirmarModal from "../componentes/ConfirmarModal";
 import "./Organizador.scss"
 
 function Tarefas() {
-    // Variáveis de estado usando o hook 'useState' para gerenciar o estado do componente
-    const [tarefas, setTarefas] = useState(todoList);
-    const [novaTarefa, setNovaTarefa] = useState("");
-    const [editandoIndex, setEditandoIndex] = useState(null);
-    const [mostrarModal, setMostrarModal] = useState(false);
-    const [deletandoIndex, setDeletandoIndex] = useState(null);
-    const [mostrarConfirmarModal, setMostrarConfirmarModal] = useState(false);
+  // Variáveis de estado usando o hook 'useState' para gerenciar o estado do componente
+  const [tarefas, setTarefas] = useState(todoList);
+  const [novaTarefa, setNovaTarefa] = useState("");
+  const [editandoIndex, setEditandoIndex] = useState(null);
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [deletandoIndex, setDeletandoIndex] = useState(null);
+  const [mostrarConfirmarModal, setMostrarConfirmarModal] = useState(false);
 
-    // Manipulador de evento para alternar o status de uma tarefa (checkbox)
-    const handleCheckboxChange = (index) => {
-        const tarefasAtualizadas = [...tarefas];
-        tarefasAtualizadas[index].status = !tarefasAtualizadas[index].status;
-        setTarefas(tarefasAtualizadas);
+  // Manipulador de evento para alternar o status de uma tarefa (checkbox)
+  const handleCheckboxChange = (index) => {
+    const tarefasAtualizadas = [...tarefas];
+    tarefasAtualizadas[index].status = !tarefasAtualizadas[index].status;
+    setTarefas(tarefasAtualizadas);
+  }
+
+  // Manipulador de evento para adicionar uma nova tarefa à lista
+  const adicionarTarefa = () => {
+    //trim() remove espaços da string. Tanto do final quanto do inicio.
+    if (novaTarefa.trim() !== "") {
+      // Cria um novo objeto representando a nova tarefa com o texto fornecido e o status inicial como falso
+      const novaTarefaObj = { tarefa: novaTarefa, status: false };
+
+      // Atualiza o estado das tarefas, adicionando a nova tarefa ao final do array
+      setTarefas([...tarefas, novaTarefaObj]);
+
+      // Limpa o campo de entrada da nova tarefa, tornando-o vazio para a próxima entrada
+      setNovaTarefa("");
     }
+  }
 
-    // Manipulador de evento para adicionar uma nova tarefa à lista
-    const adicionarTarefa = () => {
-        //trim() remove espaços da string. Tanto do final quanto do inicio.
-        if (novaTarefa.trim() !== "") {
-            // Cria um novo objeto representando a nova tarefa com o texto fornecido e o status inicial como falso
-            const novaTarefaObj = { tarefa: novaTarefa, status: false };
+  // Manipulador de evento para iniciar o modo de edição para uma tarefa
+  const handleEditarClick = (index) => {
+    setEditandoIndex(index);
+    setMostrarModal(true);
+  }
 
-            // Atualiza o estado das tarefas, adicionando a nova tarefa ao final do array
-            setTarefas([...tarefas, novaTarefaObj]);
+  // Manipulador de evento para cancelar a edição de uma tarefa
+  const handleCancelarEdicao = () => {
+    setEditandoIndex(null);
+    setMostrarModal(false);
+  }
 
-            // Limpa o campo de entrada da nova tarefa, tornando-o vazio para a próxima entrada
-            setNovaTarefa("");
-        }
-    }
+  // Manipulador de evento para confirmar a edição de uma tarefa
+  const handleConfirmarEdicao = (index, novoTexto) => {
+    const tarefasAtualizadas = [...tarefas];
+    tarefasAtualizadas[index].tarefa = novoTexto;
+    setTarefas(tarefasAtualizadas);
+    setEditandoIndex(null);
+    setMostrarModal(false);
+  }
 
-    // Manipulador de evento para iniciar o modo de edição para uma tarefa
-    const handleEditarClick = (index) => {
-        setEditandoIndex(index);
-        setMostrarModal(true);
-    }
+  // Manipulador de evento para iniciar a exclusão de uma tarefa
+  const handleDeletarClick = (index) => {
+    setDeletandoIndex(index);
+    setMostrarConfirmarModal(true);
+  }
 
-    // Manipulador de evento para cancelar a edição de uma tarefa
-    const handleCancelarEdicao = () => {
-        setEditandoIndex(null);
-        setMostrarModal(false);
-    }
+  // Manipulador de evento para cancelar a exclusão de uma tarefa
+  const handleCancelarDelecao = () => {
+    setDeletandoIndex(null);
+    setMostrarConfirmarModal(false);
+  }
 
-    // Manipulador de evento para confirmar a edição de uma tarefa
-    const handleConfirmarEdicao = (index, novoTexto) => {
-        const tarefasAtualizadas = [...tarefas];
-        tarefasAtualizadas[index].tarefa = novoTexto;
-        setTarefas(tarefasAtualizadas);
-        setEditandoIndex(null);
-        setMostrarModal(false);
-    }
+  // Manipulador de evento para confirmar a exclusão de uma tarefa
+  const handleConfirmarDelecao = () => {
+    const tarefasAtualizadas = tarefas.filter((_e, index) => index !== deletandoIndex);
+    setTarefas(tarefasAtualizadas);
+    setDeletandoIndex(null);
+    setMostrarConfirmarModal(false);
+  }
 
-    // Manipulador de evento para iniciar a exclusão de uma tarefa
-    const handleDeletarClick = (index) => {
-        setDeletandoIndex(index);
-        setMostrarConfirmarModal(true);
-    }
-
-    // Manipulador de evento para cancelar a exclusão de uma tarefa
-    const handleCancelarDelecao = () => {
-        setDeletandoIndex(null);
-        setMostrarConfirmarModal(false);
-    }
-
-    // Manipulador de evento para confirmar a exclusão de uma tarefa
-    const handleConfirmarDelecao = () => {
-        const tarefasAtualizadas = tarefas.filter((_e, index) => index !== deletandoIndex);
-        setTarefas(tarefasAtualizadas);
-        setDeletandoIndex(null);
-        setMostrarConfirmarModal(false);
-    }
-
-    // Renderizando a interface do componente
-    return (
+  // Renderizando a interface do componente
+  return (
+    <>
       <div className="topo">
-          <div className="cabeçalho">
-            <h2>Otimize seu tempo e se organize com o nosso planejador diário</h2>
-
-            {/* Mapeando as tarefas para exibi-las */}
-            {tarefas.map((e, index) => (
-                <div key={e.tarefa}>
-                    <label htmlFor="">
-                        {/* Renderização condicional para o modo de edição */}
-                        {editandoIndex === index ? (
-                            <>
-                                <input
-                                    type="text"
-                                    value={e.tarefa}
-                                    onChange={(event) => handleConfirmarEdicao(index, event.target.value)}
-                                />
-                                <button onClick={handleCancelarEdicao}>Cancelar</button>
-                                <button onClick={() => handleConfirmarEdicao(index, e.tarefa)}>Confirmar</button>
-                            </>
-                        ) : (
-                            <>
-                                {e.tarefa}
-
-                                {/* Checkbox para o status da tarefa */}
-                                <input
-                                    type="checkbox"
-                                    name=""
-                                    id=""
-                                    checked={e.status}
-                                    onChange={() => handleCheckboxChange(index)}
-                                />
-
-                                {/* Botões para editar e excluir tarefas */}
-                                <button onClick={() => handleEditarClick(index)}>Editar</button>
-                                <button onClick={() => handleDeletarClick(index)}>Deletar</button>
-                            </>
-                        )}
-                    </label>
-                </div>
-            ))}
-
-            {/* Entrada e botão para adicionar uma nova tarefa */}
-            <div>
-                <input
-                    type="text"
-                    placeholder="Adicionar uma nova tarefa"
-                    value={novaTarefa}
-                    onChange={(e) => setNovaTarefa(e.target.value)}
-                />
-                <button onClick={adicionarTarefa}> + </button>
-            </div>
-
-            {/* Modal para editar tarefas */}
-            <Modal
-                mostrar={mostrarModal}
-                handleClose={() => setMostrarModal(false)}
-                handleConfirmar={(novoNome) => handleConfirmarEdicao(editandoIndex, novoNome)}
-            />
-
-            {/* Modal para confirmar a exclusão de tarefas */}
-            <ConfirmarModal
-                mostrar={mostrarConfirmarModal}
-                handleClose={handleCancelarDelecao}
-                handleConfirmar={handleConfirmarDelecao}
-            />
-        </div>
+        <h2>Organização</h2>
+        <label><h3>Tarefa</h3></label>
+        
       </div>
+      <div className="cabeçalho">
+        <h2>Otimize seu tempo e se organize com o nosso planejador diário</h2>
+
+        {/* Mapeando as tarefas para exibi-las */}
+        {tarefas.map((e, index) => (
+          <div key={e.tarefa}>
+            <label htmlFor="">
+              {/* Renderização condicional para o modo de edição */}
+              {editandoIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={e.tarefa}
+                    onChange={(event) => handleConfirmarEdicao(index, event.target.value)}
+                  />
+                  <button onClick={handleCancelarEdicao}>Cancelar</button>
+                  <button onClick={() => handleConfirmarEdicao(index, e.tarefa)}>Confirmar</button>
+                </>
+              ) : (
+                <>
+                  {e.tarefa}
+
+                  {/* Checkbox para o status da tarefa */}
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    checked={e.status}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+
+                  {/* Botões para editar e excluir tarefas */}
+                  <button onClick={() => handleEditarClick(index)}>Editar</button>
+                  <button onClick={() => handleDeletarClick(index)}>Deletar</button>
+                </>
+              )}
+            </label>
+          </div>
+        ))}
+
+        {/* Entrada e botão para adicionar uma nova tarefa */}
+        <div>
+          <input
+            type="text"
+            placeholder="Adicionar uma nova tarefa"
+            value={novaTarefa}
+            onChange={(e) => setNovaTarefa(e.target.value)}
+          />
+          <button onClick={adicionarTarefa}> + </button>
+        </div>
+
+        {/* Modal para editar tarefas */}
+        <Modal
+          mostrar={mostrarModal}
+          handleClose={() => setMostrarModal(false)}
+          handleConfirmar={(novoNome) => handleConfirmarEdicao(editandoIndex, novoNome)}
+        />
+
+        {/* Modal para confirmar a exclusão de tarefas */}
+        <ConfirmarModal
+          mostrar={mostrarConfirmarModal}
+          handleClose={handleCancelarDelecao}
+          handleConfirmar={handleConfirmarDelecao}
+        />
+      </div>
+      </>
         
     )
 }
